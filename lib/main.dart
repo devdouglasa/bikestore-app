6633196ad93ee872811d1f8beba781ab0cef555b
devdouglasa <duglasalisson27@gmail.com>
2025-04-04 10:16:38 -0300

    Feat: edited app bar padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:bikestore/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Bike Shopping App',
       theme: ThemeData(
         primaryColorDark: Colors.white,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
       home: Scaffold(
         appBar: AppBar(
@@ -24,10 +26,20 @@ class MyApp extends StatelessWidget {
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text('Choose Your Bike', style: TextStyle(color: Colors.white)),
+              Text(
+                'Choose Your Bike',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
               IconButton(
                 style: IconButton.styleFrom(
-                  backgroundColor: Colors.blueAccent
+                  backgroundColor: Colors.blueAccent.withValues(alpha: 0.8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
                 ),
                 onPressed: () {},
                 icon: Icon(Icons.search, color: Colors.white),
