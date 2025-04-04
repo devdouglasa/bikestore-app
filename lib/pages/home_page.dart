243122ee7e133bde27504ea99603f4b6235890f5
devdouglasa <duglasalisson27@gmail.com>
2025-04-04 10:15:59 -0300

    Feat: created home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bikestore/components/card_product.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,7 +7,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Color(0xff252D3C),
+      width: double.infinity,
+      height: double.infinity,
+      color: Color(0xff252D3C),
+      child: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 15,
+        mainAxisSpacing: 30,
+        padding: EdgeInsets.only(top: 15,left: 10, right: 10, bottom: 40),
+        children: [
+          CardProduct(),
+          CardProduct(),
+          CardProduct(),
+          CardProduct(),
+          CardProduct(),
+          CardProduct(),
+          CardProduct(),
+        ],
+      )
     );
   }
 }
